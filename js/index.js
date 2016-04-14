a3560ebe59a705f45c6de9c5009a26094fa0111c
Daniel Pino <dannyp32@live.com>
2016-04-14 00:23:09 -0700

    Modified party item.

--- js/index.js
+++ js/index.js
@@ -1,24 +1,34 @@
 $('.add-party').click(function (event) {
-    $('.add-party-form2').show(function(){
-          $(".add-party-form2").slideDown();
-      });
-});
-$('.add-party-submit').click(function (event) {
-    $('.add-party-submit').attr("disabled", true);
+    if ($('.add-party').hasClass('submit')) {
+        console.log('does this get called');
+        $('.add-party.submit').attr("disabled", true);
 
-    // do ajax call here...
-    $.post("http://localhost:3000/addParty", {
-        name: $('.add-party-form > .name').val(),
-        size: $('.add-party-form > .size').val(),
-        mobile: $('.add-party-form > .mobile').val()
-    }, function (data) {
-        console.log(data);
-        // when ajax call is done 
-        $('.add-party-form').hide();
-        $('.add-party-submit').attr("disabled", false);
-    });
+        $.post("http://localhost:3000/addParty", {
+            name: $('.add-party-form2 > .name').val(),
+            size: $('.add-party-form2 > .size').val(),
+            mobile: $('.add-party-form2 > .mobile').val()
+        }, function (data) {
+            console.log(data);
+            // when ajax call is done 
+            $('.add-party-form2').hide();
+            $('.add-party.submit').attr("disabled", false);
+        });
+
+        $('.add-party-form2').hide();
+        $('.add-party.submit').attr("disabled", false);
+    } else {
+        $('.add-party-form2').show(function () {
+            $(".add-party").text('Done!');
+            $(".add-party").addClass('submit');
+            $(".add-party-form2").slideDown();
+        });
+    }
 });
 
+// Helper 
+var stringifyDate = function (date) {
+    return ((date.getHours() + 11) % 12 + 1) + ":" + date.getMinutes();
+}
 var socket = io.connect('http://localhost:3000');
 
 socket.on('connect', function () {
@@ -26,26 +36,15 @@ socket.on('connect', function () {
 });
 
 
-socket.on('new party', function (party) {
+socket.on('newParty', function (data) {
+    var party = data.party;
+    var message = data.message;
+
     console.log('got the new party socket message');
-    /*
-        var newParty = document.createElement('div');
-        var name = document.createElement('div');
-        var preview = document.createElement('div');
-        var size = document.createElement('div');
-    */
+
     var name = party.name;
     var preview = party.preview;
     var size = party.size;
-    /*
-        newParty.appendChild(name);
-        newParty.appendChild(preview);
-        newParty.appendChild(size);
 
-        document.getElementById('parties').appendChild(newParty);
-        console.log(newParty);
-        */
-
-    $('#parties').prepend('<div class=' + '"party active"' + '><div class=' + '"name"' + '>' + name + '</div><div class=' + '"preview truncate"' + '>' + preview + '</div><div class="arrival-time">1:34 PM</div><div class="size"><i class="fa fa-users icon"></i><div class="number">' + size + '</div><div class="clear"></div></div></div>');
-
+    $('#parties').prepend('<div class=' + '"party active"' + '><div class=' + '"name"' + '>' + party.name + '</div><div class=' + '"preview truncate"' + '>' + message.message + '</div><div class="arrival-time">' + stringifyDate(new Date(party.arrival_time)) + '</div><div class="size"><i class="fa fa-users icon"></i><div class="number">' + party.size + '</div><div class="clear"></div></div></div>');
 });
